c24b6a521ea19b771d9b0434aa6fb748554ccf61
Bidisha Dey <deybidisha20@gmail.com>
2025-08-17 18:08:15 +0530

    Add files via upload

---
+++ 12.08.25/assignment5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char a;
+    printf("Enter a character to get its ASCII value : ");
+    scanf("%c",&a);
+    printf("The ASCII value is \n");
+    printf("%d \n", a);
+    return 0;
+}
